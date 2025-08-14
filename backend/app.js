a40cb3bf0171bd884319a9e2867eb9ce9be63451
Raymond <raymondtdev@gmail.com>
2025-08-14 22:59:13 +0200

    deployment minor fix

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,10 @@ const PORT = process.env.PORT || 4000;
 
 //middleware
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
-
-  app.use('/api/posts', require('./routes/posts'));
+  app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 };
 app.use(cookieParser());
