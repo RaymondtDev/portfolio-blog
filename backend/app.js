a641243908cf8dd57240d14a805b86481f3dab77
Raymond <raymondtdev@gmail.com>
2025-08-16 16:21:13 +0200

    cors update for frontend

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,10 @@ const PORT = process.env.PORT || 4000;
 
 //middleware
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ 
+  origin: ["http://localhost:5173", "https://portfolio-blog-coral-nine.vercel.app"],
+  credentials: true 
+}));
 // app.use(helmet());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
